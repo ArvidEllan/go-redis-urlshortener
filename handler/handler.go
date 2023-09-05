59b450861c5832532d2adbf46b52e2890225f1fc
“ArvidEllan” <ellanwambugu6@gmail.com>
2023-09-05 09:07:25 +0300

    handler logic update

--- handler/handler.go
+++ handler/handler.go
@@ -1,15 +1,17 @@
-package handler 
+package handler
 
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/fasthttp/router"
-	"github.com/valyala/fasthttp"
 	"log"
 	"net/http"
 	"net/url"
 	"time"
 	"urlShortener/storage"
+
+	"github.com/fasthttp/router"
+	"github.com/valyala/fasthttp"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 )
 
 func New(schema string, host string , storage storage.service) *router.Router{
@@ -43,5 +45,51 @@ func responseHandler(h func(ctx *fasthttp.RequestCtx) (interface{}, int, error))
 	}
 }
 
-func (h handler) encode (ctx *fasthttp.RequestCtx) (interface{})
+func (h handler) encode (ctx *fasthttp.RequestCtx) (interface{}, int , error){
+	var input struct {
+		URL string `json:"url"`
+		Expires string `json:"expires"`
+	}
+	if err := json.Unmarshal(ctx.PostBody(), &input); err != nil {
+		return nil, http.StatusBadRequest, fmt.Errorf("Unable to decode json request body :%v", err)
+	}
+
+	uri , err := url.ParseRequestURI(input.URL)
+
+	if err != nil {
+		return nil, http.StatusBadRequest, fmt.Errorf("Invalid URL")
+	}
+
+	layoutISO := "2006-01-02 15:04:05"
+	expires , err := time.Parse(layoutISO, input.Expires)
+	if err != nil {
+		return nil,http.StatusBadRequest, fmt.Errorf("invalid expiration date")
+	}
+
+	c , err := h.storage.Save(uri.String(),expires)
+	if err != nil {
+		return nil, http.StatusBadRequest,fmt.Errorf("could not store in database: %v",err)
+	}
+
+	u := url.URL {
+		Scheme:  h.schema,
+		Host: h.host,
+		Path: C}
+	fmt.Printf("Generated Link: %v \n", u.String())
+
+	return u.String(), http.StatusCreated , nil
+}
+func (h handler) decode(ctx *fasthttp.RequestCtx) (interface{}, int,error){
+	code := ctx.UserValue("shortlink").(string)
+
+	model , err := h.storage.LoadInfo(code)
+	if err != nil {
+		ctx.Response.Header.Set("Content-Type", "application/json")
+		ctx.Response.SetStatusCode(http.StatusNotFound)
+		return
+	}
+	ctx.Redirect(uri,http.StatusMoved)
+}
+
+
 
